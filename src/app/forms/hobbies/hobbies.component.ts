c98ea42ad5d05ded5a5b54ab734d2157f6a71c3f
memory660 <poub660@gmail.com>
2021-12-07 16:14:15 +0100

    first commit

--- src/app/forms/hobbies/hobbies.component.ts
+++ src/app/forms/hobbies/hobbies.component.ts
@@ -1,25 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ControlContainer, FormGroup } from '@angular/forms';
+import { Component, Input, OnInit } from "@angular/core";
+import { ControlContainer, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-hobbies',
-  templateUrl: './hobbies.component.html',
-  styleUrls: ['./hobbies.component.scss']
+  selector: "app-hobbies",
+  templateUrl: "./hobbies.component.html",
+  styleUrls: ["./hobbies.component.scss"],
 })
 export class HobbiesComponent implements OnInit {
   public hobbiesForm: FormGroup;
   @Input() parentForm: FormGroup;
 
-  constructor(private controlContainer: ControlContainer) { }
+  // L'utilisation du ControlContainer permet de transmettre un formulaire parent à ses sous-formulaires qui sont mis en œuvre en tant que composants.
+  constructor(private controlContainer: ControlContainer) {}
 
   public ngOnInit(): void {
     this.hobbiesForm = this.controlContainer.control as FormGroup;
   }
 
-
   public logForms(): void {
-    console.log('Hobbies form', this.hobbiesForm);
-    console.log('Parent (Hero) form', this.parentForm);
+    console.log("Hobbies form", this.hobbiesForm);
+    console.log("Parent (Hero) form", this.parentForm);
   }
-
 }
